dc03d88f841e333bcdf32cdaca8166df9b1568a1
Jacquese <jacqwhitson@gmail.com>
2023-03-25 21:00:16 -0400

    Initial Commit

---
+++ FireBaseAllThings/FireBaseAllThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBaseAllThingsApp.swift
+//  FireBaseAllThings
+//
+//  Created by Jacquese Whitson  on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct FireBaseAllThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
